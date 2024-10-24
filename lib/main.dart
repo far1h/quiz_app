909d1d87800e8724a4807f5dd5f886ff7f0e8bdd
far1h <farihmuhammad45@gmail.com>
2024-10-25 01:05:43 +0700

    Add Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     home: Scaffold(
-      body: Text("Hello World"),
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 50, 18, 105),
+            Color.fromARGB(255, 60, 36, 102),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        )),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset(
+                'assets/images/quiz-logo.png',
+                width: 300,
+              ),
+              const SizedBox(
+                height: 80,
+              ),
+              const Text(
+                "Learn Flutter the fun way",
+                style: TextStyle(
+                    color: Color.fromARGB(255, 217, 217, 217), fontSize: 24),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              OutlinedButton(
+                onPressed: () {},
+                child: Text("Start Quiz"),
+                style: OutlinedButton.styleFrom(foregroundColor: Colors.white ),
+              )
+            ],
+          ),
+        ),
+//
+      ),
     ),
   ));
 }
