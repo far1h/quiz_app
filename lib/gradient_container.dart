d91e9a2eddd930328587b5ae02586c90f9f4ad4f
far1h <farihmuhammad45@gmail.com>
2024-10-26 19:01:28 +0700

    Rendering Content Conditionally

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -2,10 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/questions_screen.dart';
 import 'package:quiz_app/start_screen.dart';
 
-class GradientContainer extends StatelessWidget {
+class GradientContainer extends StatefulWidget {
   const GradientContainer({
     super.key,
   });
+
+  @override
+  State<GradientContainer> createState() => _GradientContainerState();
+}
+
+class _GradientContainerState extends State<GradientContainer> {
+  Widget activeScreen = const StartScreen();
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,8 +32,8 @@ class GradientContainer extends StatelessWidget {
         begin: Alignment.topLeft,
         end: Alignment.bottomRight,
       )),
-      child: const Center(
-        child: QuestionsScreen(),
+      child: Center(
+        child: activeScreen,
       ),
     );
   }
